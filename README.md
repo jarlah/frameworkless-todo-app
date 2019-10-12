5e793d6549e9d03f6369f9b3128993a51ed2d999
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-10-12 22:19:43 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Frameworkless todo app
+
+No webpack, no transpiling, no nothing. 
+
+This is a todo app with no framework whatsoever. 
+
+I have only used JavaScript, css and html.
+
+## How to run
+
+    npm i -g serve
+    serve -s .
+
+open http://localhost:5000
---
+++ app.js
@@ -0,0 +1,82 @@
+const todoInput = getById("todoInput");
+const todoButton = getById("todoButton");
+const todoList = getByQuery("todoList");
+
+function addTodo(todoText) {
+    const action = { type: "add", payload: todoText };
+    state = reducer(state, action);
+}
+
+function onAddTodo() {
+    addTodo(todoInput.value);
+    todoInput.value = "";
+    renderTodos();
+    todoInput.focus();
+}
+
+function removeTodo(id) {
+    const action = { type: "remove", payload: id };
+    state = reducer(state, action);
+}
+
+function onRemoveTodo(id) {
+    removeTodo(id);
+    renderTodos();
+    todoInput.focus();
+}
+
+// Event listeners
+todoButton.addEventListener("click", onAddTodo);
+todoInput.addEventListener('keypress', function (e) {
+    const key = e.which || e.keyCode;
+    if (key === 13) {
+        onAddTodo()
+    }
+});
+
+// Render operations
+function renderTodos() {
+    todoList.innerHTML = state.todos.map(createTodo).join("\n");
+}
+
+function createTodo(todo) {
+    return `<div onclick="onRemoveTodo(${todo.id})" class="todo"># ${todo.id} : ${todo.text}</div>`;
+}
+
+// Dom operations
+function getById(name) {
+    return document.getElementById(name);
+}
+
+function getByQuery(query) {
+    return document.querySelector(query);
+}
+
+// Reducer stuff
+const initialStsate = {
+    nextId: 1,
+    todos: []
+};
+
+function reducer(state = initialStsate, action) {
+    switch (action.type) {
+        case "remove":
+            return {
+                ...state,
+                todos: state.todos.filter(todo => todo.id !== action.payload)
+            };
+        case "add":
+            return {
+                ...state,
+                todos: [
+                    ...state.todos,
+                    { id: state.nextId, text: action.payload }
+                ],
+                nextId: state.nextId + 1
+            };
+        default:
+            return state;
+    }
+}
+
+let state = reducer(undefined, {});
